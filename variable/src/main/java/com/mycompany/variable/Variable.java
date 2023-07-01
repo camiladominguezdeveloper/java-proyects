39f39f12b6eb0a404afa1d150935e679b7127f09
Camila Dominguez <127351679+camiladominguezdeveloper@users.noreply.github.com>
2023-07-01 20:04:24 -0300

    Create firsts java proyects: -HolaMundo -variable

---
+++ variable/src/main/java/com/mycompany/variable/Variable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variable;
+
+/**
+ *
+ * @author Camila
+ */
+public class Variable {
+
+    public static void main(String[] args) {
+        int num1, num2, resultado;
+        
+        num1 = 4;
+        num2 = 2;
+        resultado = num1 / num2;
+        System.out.println("El resultado de la división es de: " + resultado);
+    }
+}
