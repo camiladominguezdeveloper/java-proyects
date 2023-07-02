f5809a3f7f76640afefc278126a79b408039b751
Camila Dominguez <127351679+camiladominguezdeveloper@users.noreply.github.com>
2023-07-01 22:24:17 -0300

    Update condicionalIf

---
+++ condicionalIf/src/main/java/com/mycompany/condicionalif/CondicionalIf.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.condicionalif;
+
+/**
+ *
+ * @author Camila
+ */
+public class CondicionalIf {
+
+    public static void main(String[] args) {
+        int num1=15;
+        int num2=10;
+        
+        if (num2 > num1) {
+            System.out.println("El num2 es mayor al num1");
+        }
+        else {
+            System.out.println("El num1 es mayor al num2");
+        }
+    }
+}
