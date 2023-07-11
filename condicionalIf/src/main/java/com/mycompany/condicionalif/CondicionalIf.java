fc9f993dd67e5fd6a662b38974e948d46553d8aa
Camila Dominguez <127351679+camiladominguezdeveloper@users.noreply.github.com>
2023-07-11 12:55:21 -0300

    Edit CondicionalIf.java

--- condicionalIf/src/main/java/com/mycompany/condicionalif/CondicionalIf.java
+++ condicionalIf/src/main/java/com/mycompany/condicionalif/CondicionalIf.java
@@ -11,14 +11,20 @@ package com.mycompany.condicionalif;
 public class CondicionalIf {
 
     public static void main(String[] args) {
-        int num1=15;
+        int num1=10;
         int num2=10;
         
         if (num2 > num1) {
             System.out.println("El num2 es mayor al num1");
         }
         else {
-            System.out.println("El num1 es mayor al num2");
+            
+           if (num1 == num2){
+            System.out.println("num1 y num2 son iguales");
+           }
+           else{
+               System.out.println("El num1 es mayor al num2");
+           }
         }
     }
 }
