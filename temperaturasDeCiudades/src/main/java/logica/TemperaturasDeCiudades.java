1c9fd55f501bb32013c0ae20549b3180a303fea8
Camila Dominguez <127351679+camiladominguezdeveloper@users.noreply.github.com>
2023-08-18 01:02:07 -0300

    new projects: maraton recorridoNombres temperaturasDeCiudades

---
+++ temperaturasDeCiudades/src/main/java/logica/TemperaturasDeCiudades.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica;
+import igu.pantalla;
+
+/**
+ *
+ * @author Camila
+ */
+public class TemperaturasDeCiudades {
+
+    public static void main(String[] args) {
+        pantalla panta = new pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
